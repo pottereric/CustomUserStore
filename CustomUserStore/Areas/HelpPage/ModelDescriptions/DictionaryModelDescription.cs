5b4d944e74c713b7574750100252e01f8904218b
pottereric <eric.b.potter@gmail.com>
2014-05-15 14:35:26 -0400

    Initial Solution Creation - no mods

---
+++ CustomUserStore/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CustomUserStore.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ CustomUserStore/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CustomUserStore.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
