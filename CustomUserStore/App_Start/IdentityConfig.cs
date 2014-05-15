cc78416c2d70d10b9af10cc4fe1bc654bd1675bc
pottereric <eric.b.potter@gmail.com>
2014-05-15 16:47:35 -0400

    Working Version

--- CustomUserStore/App_Start/IdentityConfig.cs
+++ CustomUserStore/App_Start/IdentityConfig.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using CustomUserStore.Models;
+using CustomUserStore.App_Start;
 
 namespace CustomUserStore
 {
@@ -18,7 +19,9 @@ namespace CustomUserStore
 
         public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context)
         {
-            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
+            //var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
+            var manager = new ApplicationUserManager(new StubUserStore<ApplicationUser>());
+
             // Configure validation logic for usernames
             manager.UserValidator = new UserValidator<ApplicationUser>(manager)
             {
---
+++ CustomUserStore/App_Start/StubUserStore.cs
@@ -0,0 +1,67 @@
+﻿using CustomUserStore.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CustomUserStore.App_Start
+{
+    public class StubUserStore<TUser> : IUserStore<TUser>, IUserStore<TUser, string>, IUserPasswordStore<TUser>, IUserPasswordStore<TUser, string>, IDisposable where TUser : global::Microsoft.AspNet.Identity.EntityFramework.IdentityUser
+    {
+        public Task CreateAsync(TUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(TUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TUser> FindByIdAsync(string userId)
+        {
+            TUser user = new ApplicationUser() { Id = "1", Email = "Alice@mail.com", UserName = "Alice@mail.com" } as TUser;
+
+            return Task.FromResult<TUser>(user);
+        }
+
+        public Task<TUser> FindByNameAsync(string userName)
+        {
+            TUser user = new ApplicationUser() { Id = "1", Email = "Alice@mail.com", UserName = "Alice@mail.com" } as TUser;
+
+            return Task.FromResult<TUser>(user); 
+        }
+
+        public Task UpdateAsync(TUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetPasswordHashAsync(TUser user)
+        {
+
+            user.PasswordHash = "AED7HfKdNxNU0C/ZsWqcSlirrdFqQ2HBiquLnKycTMj6GQ09FbNuDNSID2A589TDhw==";
+
+            return Task.FromResult(user.PasswordHash);
+        }
+
+        public Task<bool> HasPasswordAsync(TUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetPasswordHashAsync(TUser user, string passwordHash)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
